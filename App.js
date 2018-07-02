66bdb874065f1c8b7f642e4c1ce0cc88f81470ad
Tatiana Korkach <tatianakorkach@Tatianas-MBP.fritz.box>
2018-07-02 23:08:05 +0200

    added picture

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, TextInput, View } from 'react-native';
+import { AppRegistry, TextInput, View, Image } from 'react-native';
 import ListItem from './components/ListItem.js';
 import MenuBar from './components/MenuBar.js';
 
@@ -10,20 +10,26 @@ export default class ShoppingList extends Component {
   }
 
   render() {
+    let pic = {
+      uri: 'https://www.cats.org.uk/uploads/images/news/9ca9318d-961d-4c7d-9dc0-6c2502a79f86.jpg'
+    };
     return (
     <View style={{flexDirection: 'column'}}>
       <MenuBar/>
       <View style={{flexDirection: 'column'}}>      
         <View>
             <TextInput
-            style={{height: 40, fontSize: 24}}
-            placeholder="Title"
+            style={{height: 50, fontSize: 24, padding: 10}}
+            placeholder="Cats shopping list"
             onChangeText={(text) => this.setState({text})}
           />
         </View>
-        <ListItem/>
-        <ListItem/>
-        <ListItem/>
+        <View>
+          <ListItem/>
+          <ListItem/>
+          <ListItem/>
+        </View>
+        <Image source={pic} style={{width: 200, height: 200}}/>
       </View>
     </View>
     );
